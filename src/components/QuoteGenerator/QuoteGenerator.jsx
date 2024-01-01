3cff48a2a439a693dd31f7c469f167ae2011fe24
André De' Carli <a.decarli89@gmail.com>
2024-01-01 15:53:45 -0300

    implement quote generator

--- src/components/QuoteGenerator/QuoteGenerator.jsx
+++ src/components/QuoteGenerator/QuoteGenerator.jsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import "./QuoteGenerator.css"
+
 export default function QuoteGenerator() {
-  return <p>To do</p>
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://type.fit/api/quotes")
+      .then((response) => {
+        setQuotes(response.data);
+        setQuote(response.data[0]);
+      })
+  }, []);
+
+  function randomQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+  }
+
+  function getNewQuote() {
+    setQuote(randomQuote());
+  }
+
+
+  return (
+    <>
+      <h2>Mini Project 3: Quote Generator</h2>
+      <div className="quote">
+        <button onClick={getNewQuote}>Random Quote</button>
+        <p>{quote?.text}</p>
+        <p>{quote?.author}</p>
+      </div>
+    </>
+    )
 }
